997c275812cd1333ee39b766a125f9651ea6b2a9
Lucas <lucashen@gmail.com>
2020-06-09 12:19:13 -0300

    alteração no link e nomes das páginas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,278 @@
 import React from 'react';
-import Paciente from './patient';
-import Profissional from './professional';
-import Auth from '../../shared/auth';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
-export default function Home() {
-    
-	const role = parseInt(Auth.getRoleId(), 10);
+function preventDefault(event) {
+	event.preventDefault();
+}
+  
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+		width: '100%',
+	},
+	card: {
+		backgroundColor: theme.palette.background.paper,
+	}
+}));
 
-	return (
-		<div>
-			{ (role === 1) && (
-				<Profissional />
-			)}
+export default function VerticalLinearStepper() {
+    const classes = useStyles();
 
-			{ (role === 2) && (
-				<Paciente />
-			)}
-		</div>
-  	);
+    return (
+		<React.Fragment>
+			<Grid container className={classes.root} spacing={1}>
+				<Grid item xs={12}>
+					<Typography variant="h6">
+						Administrativo
+					</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Total
+							</Typography>
+							<Typography component="p" variant="h4">
+								60
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Motivo Legal
+							</Typography>
+							<Typography component="p" variant="h4">
+								3
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h4" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Grupo Risco
+							</Typography>
+							<Typography component="p" variant="h4">
+								9
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								CEAP
+							</Typography>
+							<Typography component="p" variant="h4">
+								14
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								CEAB
+							</Typography>
+							<Typography component="p" variant="h4">
+								8
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={12} style={{marginTop: 30}}>
+					<Typography variant="h6">
+						Peritos
+					</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Total
+							</Typography>
+							<Typography component="p" variant="h4">
+								60
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Motivo Legal
+							</Typography>
+							<Typography component="p" variant="h4">
+								3
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h4" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Grupo Risco
+							</Typography>
+							<Typography component="p" variant="h4">
+								9
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={12} style={{marginTop: 30}}>
+					<Typography variant="h6">
+						Assistente Social
+					</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Total
+							</Typography>
+							<Typography component="p" variant="h4">
+								60
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Motivo Legal
+							</Typography>
+							<Typography component="p" variant="h4">
+								3
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h4" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Grupo Risco
+							</Typography>
+							<Typography component="p" variant="h4">
+								9
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={12} style={{marginTop: 30}}>
+					<Typography variant="h6">
+						Estagiários
+					</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Total
+							</Typography>
+							<Typography component="p" variant="h4">
+								60
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Motivo Legal
+							</Typography>
+							<Typography component="p" variant="h4">
+								3
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h4" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Grupo Risco
+							</Typography>
+							<Typography component="p" variant="h4">
+								9
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={12} style={{marginTop: 30}}>
+					<Typography variant="h6">
+						Temporários
+					</Typography>
+				</Grid>
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Total
+							</Typography>
+							<Typography component="p" variant="h4">
+								60
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h2" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Motivo Legal
+							</Typography>
+							<Typography component="p" variant="h4">
+								3
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={2}>
+					<Card className={classes.card}>
+						<CardContent>
+							<Typography component="h4" variant="subtitle2" color="primary" gutterBottom>
+								Afast. Grupo Risco
+							</Typography>
+							<Typography component="p" variant="h4">
+								9
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+			</Grid>
+			
+		</React.Fragment>
+    );
 }
--- src/pages/MainRoutes.js
+++ src/pages/MainRoutes.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Switch, Redirect } from 'react-router';
 import PrivateRoute from '../PrivateRoute';
 import Home from './Home';
-import Historico from './Historico';
-import Atendimento from './Atendimento';
+import Form from './Form';
 
 const MainRoutes = () => {
     return (
         <Switch>
             <PrivateRoute exact path='/' component={Home} />
-            <PrivateRoute exact path='/historico' component={Historico} />
-            <PrivateRoute exact path='/atendimento/:id' component={Atendimento} />
+            <PrivateRoute exact path='/form' component={Form} />
             <Redirect from='*' to='/' />
         </Switch>
     );
