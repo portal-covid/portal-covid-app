43727dfb47f4e964f304ef7b91c7234a1361e575
Lucas <lucashen@gmail.com>
2020-06-10 12:44:16 -0300

    troca nome da função

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -38,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
 	}
 }));
 
-export default function VerticalLinearStepper() {
+export default function Home() {
 	const { enqueueSnackbar } = useSnackbar();
 	const classes = useStyles();
 	const history = useHistory();
