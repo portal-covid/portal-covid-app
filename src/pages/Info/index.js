5ce1c4cbb58c087c37e7b4fd754de33104e094e3
Lucas <lucashen@gmail.com>
2020-06-10 12:44:48 -0300

    add botão para alterar dados

--- src/pages/Info/index.js
+++ src/pages/Info/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-
-function preventDefault(event) {
-	event.preventDefault();
-}
+import { useSnackbar } from 'notistack';
+import Button from '@material-ui/core/Button';
   
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -17,16 +15,38 @@ const useStyles = makeStyles((theme) => ({
 	},
 	card: {
 		backgroundColor: theme.palette.background.paper,
-	}
+    },
+    divButton: {
+        width: '100%',
+        textAlign: 'right'
+    }
 }));
 
-export default function VerticalLinearStepper() {
-    const classes = useStyles();
+export default function Info() {
+    const { enqueueSnackbar } = useSnackbar();
+    const location = useLocation();
+	const classes = useStyles();
+    const history = useHistory();
+    //const [dados, setDados] = useState(location.state.detail);
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        history.push('/form');
+    }
 
     return (
 		<React.Fragment>
 			<Grid container className={classes.root} spacing={1}>
-				<Grid item xs={12}>
+                <Grid item xs={12} className={classes.divButton}>
+                <Button 
+                    size="small" 
+                    variant="outlined" 
+                    color="primary"
+                    onClick={handleChange}>
+                    Alterar Dados
+                </Button>
+				</Grid>
+                <Grid item xs={12}>
 					<Typography variant="h6">
 						Administrativo
 					</Typography>
