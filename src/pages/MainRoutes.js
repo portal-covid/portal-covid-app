9b415951ffac471636611ea82bde9bda04c49cf8
Lucas <lucashen@gmail.com>
2020-06-10 10:57:07 -0300

    acrescenta a rota info

--- src/pages/MainRoutes.js
+++ src/pages/MainRoutes.js
@@ -3,11 +3,13 @@ import { Switch, Redirect } from 'react-router';
 import PrivateRoute from '../PrivateRoute';
 import Home from './Home';
 import Form from './Form';
+import Info from './Info';
 
 const MainRoutes = () => {
     return (
         <Switch>
             <PrivateRoute exact path='/' component={Home} />
+            <PrivateRoute exact path='/info' component={Info} />
             <PrivateRoute exact path='/form' component={Form} />
             <Redirect from='*' to='/' />
         </Switch>
