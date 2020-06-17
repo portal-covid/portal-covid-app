1cbaeab998810d7fa6c18eb7978ade3517d070db
israel <israel.souza@inss.gov.br>
2020-06-17 02:59:24 -0300

    up docker

---
+++ build/precache-manifest.841dd5fdfd0b63720eeaed6a7f93b488.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9080c46fa648aca211d36918b7a4628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47a029826ebf4fbba4d4",
+    "url": "/static/css/main.5a5dcdf6.chunk.css"
+  },
+  {
+    "revision": "0c5fd2f9650d5d86a119",
+    "url": "/static/js/2.ec2b13c7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.ec2b13c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a029826ebf4fbba4d4",
+    "url": "/static/js/main.fcd74ebb.chunk.js"
+  },
+  {
+    "revision": "d8a040a81b002d74aa34",
+    "url": "/static/js/runtime-main.5e568555.js"
+  },
+  {
+    "revision": "1ff4e24e72868cca0686b1b71e80a840",
+    "url": "/static/media/logo_INSS.1ff4e24e.svg"
+  },
+  {
+    "revision": "4754b37908755eb76435bbe1c05ff992",
+    "url": "/static/media/portal.4754b379.png"
+  },
+  {
+    "revision": "d2ec468e9b7023bd3a781130f38395aa",
+    "url": "/static/media/simbolo_inss.d2ec468e.svg"
+  }
+]);
